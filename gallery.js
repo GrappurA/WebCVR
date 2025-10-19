f652d62b0e6577ddcdf0ebb963a5f99f5b5427d6
Your Name <your.email@example.com>
2025-10-20 00:33:53 +0300

    added image onclick enlargement functionality

--- gallery.js
+++ gallery.js
@@ -1,7 +1,35 @@
-const element = document.getElementById("gallery");
+//image hover animation
+const galleryImages = document.querySelectorAll("#gallery img");
+galleryImages.forEach(img => {
+    img.className += " transition-transform duration-350 hover:scale-150 border-4";
 
-for (const child of element.children) {
-    if (child.tagName == "IMG") {
-        child.className+=" transition-transform duration-350 hover:scale-160 border-4";
+});
+
+//image click enlargement
+const overlay = document.getElementById("overlay");
+const overlayImg = document.getElementById("overlay-img");
+
+galleryImages.forEach(image => {
+    if (image.id != "up_img") {
+        image.onclick = () => {
+            overlayImg.src = image.src;
+            overlayImg.classList.add("mx-auto");
+            overlayImg.classList.add("mt-[2%]");
+            overlay.classList.remove("hidden");
+        };
     }
-}
+});
+
+overlay.onclick = () => {
+    overlay.classList.add("hidden");
+    overlayImg.src = "leave.png";
+};
+
+//esc press => escape image enlargement
+document.body.addEventListener('keydown', function (e) {
+    if (e.key == "Escape") {
+        overlay.classList.add("hidden");
+        overlayImg.src = "leave.png";
+    }
+});
+
